69489b5342eaa6d7fec23a8204310f42a3e646b4
bartekoklol <88594673+bartekoklol@users.noreply.github.com>
2022-02-05 22:53:09 +0100

    barte mod FINAL update
    yayaee

--- assets/preload/data/go-hard/modchart.lua
+++ assets/preload/data/go-hard/modchart.lua
@@ -3,16 +3,13 @@ function start(song) -- do nothing
 end
 
 function update(elapsed)
-    if difficulty > 0 and curStep > 736 then
+    if curStep > 736 then
         local currentBeat = (songPos / 1000)*(bpm/60)
 		for i=0,7 do
 			setActorX(_G['defaultStrum'..i..'X'] + 128 * math.sin((currentBeat + i*0.5) * math.pi), i)
 			setActorY(_G['defaultStrum'..i..'Y'] + 128 * math.cos((currentBeat + i*0.5) * math.pi), i)
-	if difficulty > 0 and curStep == 742 then
+	if curStep == 742 then
 	setActorY(600, 'girlfriend')
-	if difficulty > 0 and curStep > 748 then
-	setHudAngle(getHudAngle() + 1)
-	            end
 		    end
 		end
 	end
